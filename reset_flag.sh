52f53a41a699a29dffde323f4e069a11833a649d
Breathing is Fun <50167941+P4rC3L@users.noreply.github.com>
2026-06-02 01:15:48 +0530

    Update reset_flag.sh

--- reset_flag.sh
+++ reset_flag.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+# How to Use? 
+# This script will delete the Check flag so the main script could run a test again. 
+# Removing one of the tools will help us see whether the visual loading bar is working as intended.
+
 rm ./.sub-recon_ran_already
 sudo apt remove sublist3r -y
